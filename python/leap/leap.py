dc84b77f2f89a55df093603ef081fbfa5157197c
apena19 <andrespe1567@gmail.com>
2020-02-29 12:00:48 -0500

    uodate soluction leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,8 +1,2 @@
 def leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0 and not year % 400 == 0:
-            return False
-        else:
-            return True
-    else:
-        return False
+    return year % 4 == 0 and not (year % 100 == 0 and not year % 400 == 0)
